f2fb0be90484965a89398b506673995e5e90eaa6
melan96 <it17006712@my.sliit.lk>
2020-01-21 08:04:11 +0530

    import of essential crpyto modules

--- file_encrypt_script.py
+++ file_encrypt_script.py
@@ -1,4 +1,15 @@
-file_location=input("File Location")
-type(file_location)
+# Author: Melan Rashitha Dias
+# Contact: melan96@github.io 
 
-print(file_location)
+# import CORE Pycrypto module for AES encryptions
+import os
+# Import AES symmetric cipher - Maintained fixed data block chuncks 16K
+from Crypto.Cipher import AES
+
+# SHA256 Secure Hash Algorithm
+from Crypto.Hash import SHA256
+
+# Random file postfixes
+from Crypto import Random
+
+
